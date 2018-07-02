ea0caf220cd561891a709a5bb9baa61e18bf2d0f
Blessing Philips <blessing.philips@sc-mb163.office.clix.io>
2018-07-02 11:26:22 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ruby-cipher
+## Caesar Cipher
+
+The **Caesar Cipher**, also known as a shift cipher, is one of the oldest and simplest forms of encrypting a message. It is a type of substitution cipher where each letter in the original message (which in cryptography is called the plaintext) is replaced with a letter corresponding to a certain number of letters shifted up or down in the alphabet. 
+
+For each letter of the alphabet, you would take its position in the alphabet, say 3 for the letter 'C', and shift it by the key number. If we had a key of +3, that 'C' would be shifted down to an 'F' - and that same process would be applied to every letter in the plaintext.
+
+In this way, a message that initially was quite readable, ends up in a form that cannot be understood at a simple glance. 
+
+![image](https://learncryptography.com/assets/content/images/caesar_cipher.jpg)
+
+For example, here's the Caesar Cipher encryption of a full message, using a left shift of 3. 
+
+`Plaintext:  
+THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG  
+
+Ciphertext:  
+QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD`
--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,29 +1,20 @@
 def caesar_cipher(string, shift)
 
-    # Splitting the string into an array
     string_array = string.split("")
 
-    # Mapping through
     string_array.map do | character |
         chr_num = character.ord
 
-        # 65 - 90 is Decimal system number for Capital letters
-        # 97 - 122 is Decimal system number for Small letters
-
         if ((chr_num >= 65 && chr_num <= 90) || (chr_num >= 97 && chr_num <= 122))
 
-            # Cipher
             shifted_chr_num = chr_num + shift
 
-            # Check if the cipher is no longer an Alphabeth
             if ((shifted_chr_num >= 91 &&  shifted_chr_num <= 96) || (shifted_chr_num > 122))
                 shifted_chr_num = shifted_chr_num - 26
             end
         else
-            # If it is not an Alphabeth
             shifted_chr_num = character
         end
-        # Covert from Decimal system number to character
         shifted_chr_num.chr
     end
     .join
@@ -31,30 +22,21 @@ end
 
 def ceasar_decipher(string, shift)
 
-    # Splitting the string into an array
     string_array = string.split("")
 
-    # Mapping through
     string_array.map do | character |
         chr_num = character.ord
 
-        # 65 - 90 is Decimal system number for Capital letters
-        # 97 - 122 is Decimal system number for Small letters
-
         if ((chr_num >= 65 && chr_num <= 90) || (chr_num >= 97 && chr_num <= 122))
 
-            # Cipher
             shifted_chr_num = chr_num - shift
             
-            # Check if the cipher is no longer an Alphabeth
             if ((shifted_chr_num < 65) || (shifted_chr_num >= 91 && shifted_chr_num <= 96))
                 shifted_chr_num = shifted_chr_num + 26
             end
         else
-            # If it is not an Alphabeth
             shifted_chr_num = character
         end
-        # Convert from Decimal system number to character
         shifted_chr_num.chr
 
     end
